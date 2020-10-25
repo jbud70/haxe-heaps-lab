499c24121e5001725c36a4740809e0b7bd9748f0
Josh Isaak <jbud70@gmail.com>
2020-10-25 11:13:57 -0400

    Added DT

--- src/Main.hx
+++ src/Main.hx
@@ -1,89 +1,88 @@
 import h2d.Anim;
 
 class Main extends hxd.App {
-    var bmp : h2d.Bitmap;
-    var spinSpeed : Float = 0.01;
+	var bmp:h2d.Bitmap;
+	var spinSpeed:Float = 1;
 
-    override function init() {
-        // allocate a Texture with red color and creates a 100x100 Tile from it
-        var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
-        // create a Bitmap object, which will display the tile
-        // and will be added to our 2D scene (s2d)
-        bmp = new h2d.Bitmap(tile, s2d);
+	override function init() {
+		// allocate a Texture with red color and creates a 100x100 Tile from it
+		var tile = h2d.Tile.fromColor(0xFF0000, 100, 100);
+		// create a Bitmap object, which will display the tile
+		// and will be added to our 2D scene (s2d)
+		bmp = new h2d.Bitmap(tile, s2d);
 
-        var spinInteraction = new h2d.Interactive(100, 100, bmp);
+		var spinInteraction = new h2d.Interactive(100, 100, bmp);
 
-        spinInteraction.visible = true;
+		spinInteraction.visible = true;
 
-        spinInteraction.onOver = function (event : hxd.Event) {
-            spinSpeed = 0.0;
-        }
+		spinInteraction.onOver = function(event:hxd.Event) {
+			spinSpeed = 0.0;
+		}
 
-        spinInteraction.onOut = function (event : hxd.Event) {
-            spinSpeed = 0.01;
-        }
+		spinInteraction.onOut = function(event:hxd.Event) {
+			spinSpeed = 1.0;
+		}
 
-        // modify the display position of the Bitmap sprite
-        bmp.x = s2d.width * 0.5;
-        bmp.y = s2d.height * 0.5;
+		// modify the display position of the Bitmap sprite
+		bmp.x = s2d.width * 0.5;
+		bmp.y = s2d.height * 0.5;
 
-        // Add animation
-        // creates three tiles with different color
-        var t1 = h2d.Tile.fromColor(0xFF0000, 30, 30);
-        var t2 = h2d.Tile.fromColor(0x00FF00, 30, 30);
-        var t3 = h2d.Tile.fromColor(0x0000FF, 30, 30);
+		// Add animation
+		// creates three tiles with different color
+		var t1 = h2d.Tile.fromColor(0xFF0000, 30, 30);
+		var t2 = h2d.Tile.fromColor(0x00FF00, 30, 30);
+		var t3 = h2d.Tile.fromColor(0x0000FF, 30, 30);
 
-        // creates an animation for these tiles
-        var anim = new h2d.Anim([t1,t2,t3],s2d);
-        anim.x = 100;
-        anim.y = 100;
-        anim.speed = 10;
+		// creates an animation for these tiles
+		var anim = new h2d.Anim([t1, t2, t3], s2d);
+		anim.x = 100;
+		anim.y = 100;
+		anim.speed = 10;
 
-        anim.pause = true;
-        
-        var interaction = new h2d.Interactive(anim.frames[0].width, anim.frames[0].height, anim);
+		anim.pause = true;
 
-        interaction.onOver = function(event : hxd.Event) {
-            toggleAnimPlay(anim);
-        }
+		var interaction = new h2d.Interactive(anim.frames[0].width, anim.frames[0].height, anim);
 
-        interaction.onOut = function(event : hxd.Event) {
-            toggleAnimPlay(anim);
-        }
+		interaction.onOver = function(event:hxd.Event) {
+			toggleAnimPlay(anim);
+		}
 
-        /* anim.loop = true;
+		interaction.onOut = function(event:hxd.Event) {
+			toggleAnimPlay(anim);
+		}
 
-        var playCount = 0;
+		/* anim.loop = true;
 
-        anim.onAnimEnd = function () {
-            playCount += 1;
+			var playCount = 0;
 
-            trace(playCount);
+			anim.onAnimEnd = function () {
+				playCount += 1;
 
-            if (playCount >= 10) {
-                // anim.play([t1,t2,t3], 0);
-                toggleAnimPlay(anim);
-            }
-        } */
+				trace(playCount);
 
-    }
+				if (playCount >= 10) {
+					// anim.play([t1,t2,t3], 0);
+					toggleAnimPlay(anim);
+				}
+		}*/
+	}
 
-    function toggleAnimPlay(anim: Anim): Void {
-        if (anim.pause == true) {
-            anim.play(anim.frames, 0);
-        }
-        else {
-            anim.pause = true;
-        }
-    }
+	function toggleAnimPlay(anim:Anim):Void {
+		if (anim.pause == true) {
+			anim.play(anim.frames, 0);
+		} else {
+			anim.pause = true;
+		}
+	}
 
-    // on each frame
-    override function update(dt:Float) {
-        // increment the display bitmap rotation by 0.1 radians
-        bmp.rotation += spinSpeed;
-    }
+	// on each frame
+	override function update(dt:Float) {
+		// increment the display bitmap rotation by 0.1 radians
+		bmp.rotation += spinSpeed * dt;
+		bmp.x += 10 * dt;
+	}
 
-    static function main() {
-        new Main();
-    }
+	static function main() {
+		new Main();
+	}
 }
